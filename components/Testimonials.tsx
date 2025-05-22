63b2e95f57febcfe18c9737541a43003d2cb9067
Arin7712 <aringawande7712@gmail.com>
2025-05-22 21:32:41 +0530

    ok

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -1,49 +1,80 @@
-import { Noto_Serif } from 'next/font/google'
-import Image from 'next/image'
-import React from 'react'
+import { Star } from "lucide-react";
+import { Noto_Serif } from "next/font/google";
+import Image from "next/image";
+import React from "react";
 
 const font = Noto_Serif({
-  subsets: ['latin'],
-  weight: ['100', '200', '300'],
-  display: 'swap',
-})
+  subsets: ["latin"],
+  weight: ["100", "200", "300"],
+  display: "swap",
+});
 
 const Testimonials = () => {
   return (
-    <div className='flex flex-col md:px-[6rem] px-6 pt-[6rem]'>
-      <div className='flex flex-col md:flex-row justify-center'>
-        <div className='flex flex-col md:gap-[4rem] gap-10 md:w-[70%]'>
-            <div className='space-y-4'>
-                <h1 className={`md:text-5xl text-3xl ${font.className} md:block hidden`}>Celebrities Testimonials</h1>
-                                <h1 className={`md:text-5xl text-3xl ${font.className} md:hidden block`}>Sun Seekers</h1>
-                <p className='text-sm text-zinc-600'>The world’s top performers PersonalNutrtion.com</p>
-            </div>
+    <div className="flex flex-col md:px-[6rem] px-6 pt-[6rem]">
+      <div className="flex flex-col md:flex-row justify-center">
+        <div className="flex flex-col md:gap-[4rem] gap-10 md:w-[70%]">
+          <div className="space-y-4">
+            <h1
+              className={`md:text-5xl text-3xl ${font.className} md:block hidden`}
+            >
+              Celebrities Testimonials
+            </h1>
+            <h1
+              className={`md:text-5xl text-3xl ${font.className} md:hidden block`}
+            >
+              Sun Seekers
+            </h1>
+            <p className="text-sm text-zinc-600">
+              The world’s top performers PersonalNutrtion.com
+            </p>
+          </div>
 
-            <div className='flex flex-wrap md:gap-10 gap-4 items-center text-sm text-zinc-600'>
-                <p>Dr.Andrew Huberman</p>
-                <p>Allyson Felix</p>
-                <p>Sir Lewis Hamilton</p>
-                <p>Jeremy Jauncey</p>
-            </div>
+          <div className="flex flex-wrap md:gap-10 gap-4 items-center text-sm text-zinc-600">
+            <p>Dr.Andrew Huberman</p>
+            <p>Allyson Felix</p>
+            <p>Sir Lewis Hamilton</p>
+            <p>Jeremy Jauncey</p>
+          </div>
 
-        <div className='md:hidden block flex justify-center'>
-            <Image src='/video1.png' alt='video' width={400} height={400} />
+          <div className="md:hidden block flex justify-center">
+            <Image src="/video1.png" alt="video" width={400} height={400} />
+          </div>
+
+          <div>
+            <p className={`${font.className} md:text-3xl md:w-[70%] text-xl`}>
+              “I’ve been using P-NUT since 2012 because it’s the simplest, most
+              straightforward way for me to get my bases of important vitamins,
+              minerals, and probiotics.”
+            </p>
+          </div>
+          <div>
+            <p className="w-[60%] text-zinc-600 md:text-md text-sm">
+              NEUROSCIENTIST, HOST OF THE HUBERMAN LAB PODCAST, Personal
+              Nutrition PARTNER
+            </p>
+          </div>
         </div>
 
-            <div>
-                <p className={`${font.className} md:text-3xl md:w-[70%] text-xl`}>“I’ve been using P-NUT since 2012 because it’s the simplest, most straightforward way for me to get my bases of important vitamins, minerals, and probiotics.”</p>
-            </div>
-            <div>
-                <p className='w-[60%] text-zinc-600 md:text-md text-sm'>NEUROSCIENTIST, HOST OF THE HUBERMAN LAB PODCAST, Personal Nutrition PARTNER</p>
-            </div>
-        </div>
-
-        <div className='md:block hidden'>
-            <Image src='/video1.png' alt='video' width={400} height={400} />
+        <div className="md:block hidden">
+          <Image src="/video1.png" alt="video" width={400} height={400} />
         </div>
       </div>
+      <div className="relative md:w-[50%] w-full aspect-[1/1] mx-auto">
+        <Image
+          src="/ellipse.png"
+          alt="sunseekers"
+          fill
+          className="object-contain"
+        />
+        <h1
+          className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:text-4xl text-3xl z-1 ${font.className}`}
+        >
+          100 Reviews With An Average Rating Of 4/5 ⭐
+        </h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
