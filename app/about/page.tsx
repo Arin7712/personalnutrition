3a35bd5b05614918e5a9c84f06c2074c69247038
Arin7712 <aringawande7712@gmail.com>
2025-06-24 15:31:25 +0530

    added section for the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,9 @@
+import About from '@/components/About'
 import FounderStory from '@/components/about/FounderStory'
 import Hero from '@/components/about/Hero'
+import Community from '@/components/Community'
+import Event from '@/components/Event'
+import Products from '@/components/Products'
 import React from 'react'
 
 const page = () => {
@@ -7,6 +11,10 @@ const page = () => {
     <div>
       <Hero/>
       <FounderStory/>
+      <About/>
+      <Products/>
+      <Event/>
+      <Community/>
     </div>
   )
 }
